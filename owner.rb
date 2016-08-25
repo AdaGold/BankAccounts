9d3567a6d2850041ec5b5b9cec18f7607afcb635
heatherherrington <redherring@gmail.com>
2016-08-25 15:42:34 -0700

    Satisfied Wave 2 Optional, except linking owner to account

--- owner.rb
+++ owner.rb
@@ -1,35 +1,70 @@
-#require_relative 'account'
+# require_relative 'account'
+require 'csv'
 
 module Bank
   class Owner
     attr_accessor
-    attr_reader :first_name, :last_name, :street_address, :city, :state, :zip_code
+    attr_reader :id, :first_name, :last_name, :street_address, :city, :state
     attr_writer
 
-    def initialize(owners, first_name, last_name, street_address, city, state, zip_code)
-      @owners = []
-      @first_name = first_name
-      @last_name = last_name
-      @street_address = street_address
-      @city = city
-      @state = state
-      @zip_code = zip_code
+    def initialize(owner_hash)
+      @id = owner_hash[:id]
+      @first_name = owner_hash[:first_name]
+      @last_name = owner_hash[:last_name]
+      @street_address = owner_hash[:street_address]
+      @city = owner_hash[:city]
+      @state = owner_hash[:state]
     end
 
     def create_owner
       puts "What is your first name?"
-      @first_name = gets.chomp
+      @owner[:first_name] = gets.chomp
       puts "What is your last name?"
-      @last_name = gets.chomp
+      @owner[:last_name] = gets.chomp
       puts "What is your street address?"
-      @street_address = gets.chomp
+      @owner[:street_address] = gets.chomp
       puts "In what city do you live?"
-      @city = gets.chomp
+      @owner[:city] = gets.chomp
       puts "In what state do you live?"
-      @state = gets.chomp
-      puts "What is your zip code?"
-      @zip_code = gets.chomp
-      @owners << owner
+      @owner[:state] = gets.chomp
+      @owners << @owner
+    end
+
+    def self.all
+      owners = []
+      CSV.read("./support/owners.csv").each do |line|
+        owner_hash = {}
+        owner_hash[:id] = line[0].to_i
+        owner_hash[:last_name] = line[1]
+        owner_hash[:first_name] = line[2]
+        owner_hash[:street_address] = line[3]
+        owner_hash[:city] = line[4]
+        owner_hash[:state] = line[5]
+        owners << Bank::Owner.new(owner_hash)
+      end
+      return owners
+    end
+
+    # Returns an instance of Account where the value of the id field
+    # in the CSV matches the passed parameter; will have to use self.all
+    def self.find(input)
+      account_owner = self.all
+      account_owner.each do |var|
+        if var.id == input
+          puts var.print_props
+          return var
+        end
+      end
+    end
+
+    def print_props
+      return "Account ID #{ @id } belongs to #{ @first_name } #{ @last_name } whose address is #{ @street_address }, #{ @city }, #{ @state }."
     end
   end
 end
+
+account_owners = Bank::Owner.all
+puts account_owners
+
+o = Bank::Owner.find(20)
+puts o
